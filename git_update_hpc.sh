45cd2214608538cb6f9efb18d3c75881d4c0eace
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-11-12 23:41:46 +0100

    HPC script git reset --hard

--- git_update_hpc.sh
+++ git_update_hpc.sh
@@ -2,7 +2,7 @@ echo "Job: $1"
 echo "Username: $2"
 
 ssh -t ${username:=sjsivert}@login.stud.ntnu.no "ssh -t idun 'cd Masteroppgave && \
-		git pull && \
+		git reset --hard HEAD && git pull && \
 		sbatch $1 && \
 		squeue -u sjsivert && \
 		tail -f batch_jobs/output/sbatch_job.log && \
