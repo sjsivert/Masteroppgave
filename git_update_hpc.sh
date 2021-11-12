3d02f049ecce306538661874ca098d816357196b
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-11-12 23:17:02 +0100

    Add follow log file hpc addjob

--- git_update_hpc.sh
+++ git_update_hpc.sh
@@ -5,4 +5,5 @@ ssh -t sjsivert@login.stud.ntnu.no "ssh -t idun 'cd Masteroppgave && \
 		git pull && \
 		sbatch $1 && \
 		squeue -u sjsivert && \
+		tail batch_jobs/output/sbatch_job.log && \
 		bash -l'"
