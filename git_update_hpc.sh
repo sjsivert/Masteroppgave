6c02660ea7f96a402fa6b4fb510d07b09653eec1
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-11-12 23:29:26 +0100

    Made HPC script login to tunable

--- git_update_hpc.sh
+++ git_update_hpc.sh
@@ -1,9 +1,9 @@
 echo "Job: $1"
-echo "Files: $2"
+echo "Username: $2"
 
-ssh -t sjsivert@login.stud.ntnu.no "ssh -t idun 'cd Masteroppgave && \
+ssh -t ${username:=sjsivert}@login.stud.ntnu.no "ssh -t idun 'cd Masteroppgave && \
 		git pull && \
 		sbatch $1 && \
 		squeue -u sjsivert && \
-		tail batch_jobs/output/sbatch_job.log && \
+		tail -f batch_jobs/output/sbatch_job.log && \
 		bash -l'"
