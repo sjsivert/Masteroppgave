5dba15c12e96e04b4e47d5bc3ea1ad064ef42755
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-11-12 23:50:56 +0100

    Fix log output

--- git_update_hpc.sh
+++ git_update_hpc.sh
@@ -1,9 +1,11 @@
 echo "Job: $1"
 echo "Username: $2"
 
+export LOG_LOCATION=batch_jobs/output/sbatch_job.log
+
 ssh -t ${username:=sjsivert}@login.stud.ntnu.no "ssh -t idun 'cd Masteroppgave && \
-		git reset --hard HEAD && git pull && \
+		git reset --hard origin && git pull origin master  && \
 		sbatch $1 && \
 		squeue -u sjsivert && \
-		tail -f batch_jobs/output/sbatch_job.log && \
+		tail -f ${LOG_LOCATION}&& \
 		bash -l'"
