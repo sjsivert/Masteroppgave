c5279f88cafe550d210981e82fecf79ebf158a6c
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-12-01 20:14:12 +0100

    C: Add try catch to load_data_from_csv

--- code/data/data_loader.py
+++ code/data/data_loader.py
@@ -6,9 +6,14 @@ from genpipes import declare, compose
 
 @declare.generator()
 def load_and_merge_market_insight_and_categories(market_insight_path: str, categories_path: str) -> pd.DataFrame:
-    df1 = pd.read_csv(market_insight_path)
-    df2 = pd.read_csv(categories_path)
-    return pd.merge(df1, df2, how="left", left_on="cat_id", right_on="internal_doc_id")
+    try:
+        df1 = pd.read_csv(market_insight_path)
+        df2 = pd.read_csv(categories_path)
+        return pd.merge(df1, df2, how="left", left_on="cat_id", right_on="internal_doc_id")
+    except Exception as e:
+        logging.exception(
+            "Unable to download training & test CSV, check your internet connection. Error: %s", e
+        )
 
 @declare.generator()
 @declare.datasource()
