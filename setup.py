ab52ab56a9781960d5648ecd627365ff568c98fb
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-12-05 22:37:27 +0100

    C: Add setup.py project file

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name='Masterproject',
+
+      version='1.0',
+
+      description='Sindre and Sanders Master project',
+
+      author='Sindre J. I. Sivertsen, Sander Kilen',
+
+      author_email='sindrejohan1@gmail.com',
+
+      url='https://github.com/NikZy/Masteroppgave/',
+
+      packages=find_packages(include=['code']),
+
+     )
