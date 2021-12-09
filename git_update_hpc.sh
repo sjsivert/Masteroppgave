2e3668b4943325102e43f7f80056e6acab8e7ed8
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-12-09 17:18:49 +0100

    HPC-init: test-run

--- git_update_hpc.sh
+++ git_update_hpc.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 echo "Job: $1"
-echo "Job experiment message: $2"
+echo "Job title: $2"
+echo "Job experiment descriptoion $3"
 
 export LOG_LOCATION=batch_jobs/output/sbatch_job.txt
 export TERM=xterm
 
+
+BRANCH_NAME = "hpc/$2"
+git checkout -b $BRANCH_NAME
 git add .
 git commit -m "HPC-init: $2"
-git push
+git push --set-upstream origin $BRANCH_NAME
+
 USERNAME=${username:=sjsivert}
 
 ssh -t ${username:=sjsivert}@login.stud.ntnu.no "ssh -t idun 'cd Masteroppgave && \
-		git reset --hard origin/master && git pull origin master  && \
+		git fetch \
+		git reset --hard origin/master \
+		git checkout origin/$BRANCH_NAME && git pull && \
 		touch $LOG_LOCATION && \
 		> $LOG_LOCATION && \
-		echo $2 >> $LOG_LOCATION && \
+		echo "$2: $3">> $LOG_LOCATION && \
 		echo "Adding job to queue" && \
-		sbatch $1 && \
+		sbatch $1 $2 $3 && \
 		export JOB_ID=$(sbatch $1 | grep -o -P '(\d{7})') && \
 		screen -d -m sh watch_for_job_finnish.sh $2 $USERNAME && \
 		squeue -u $USERNAME'"
