e3db920af9324d369617ed16b6d6494303edce3b
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-12-09 13:29:48 +0100

    C: Add python linting and formatter

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
-
 #!/usr/bin/env python
 
 from distutils.core import setup
 from setuptools import setup, find_packages
 
-setup(name='Masterproject',
-
-      version='1.0',
-
-      description='Sindre and Sanders Master project',
-
-      author='Sindre J. I. Sivertsen, Sander Kilen',
-
-      author_email='sindrejohan1@gmail.com',
-
-      url='https://github.com/NikZy/Masteroppgave/',
-
-      packages=find_packages(include=['src']),
-
-     )
+setup(
+    name="Masterproject",
+    version="1.0",
+    description="Sindre and Sanders Master project",
+    author="Sindre J. I. Sivertsen, Sander Kilen",
+    author_email="sindrejohan1@gmail.com",
+    url="https://github.com/NikZy/Masteroppgave/",
+    packages=find_packages(include=["src"]),
+)
