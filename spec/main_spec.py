f2bb6300f56fa0b5e37b4be462a3ac18b8a2f108
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-12-10 20:12:06 +0100

    C: Fix SonarCloud code smells

--- spec/main_spec.py
+++ spec/main_spec.py
@@ -1,16 +1,13 @@
 from confuse.exceptions import NotFoundError
-from expects import be_false, be_true, expect
+from expects import be_true, expect
 from mamba import description, it
 from mockito import mock, when
-from src import main
-from src.utils.config_parser import config, get_absolute_path
+from src.utils.config_parser import get_absolute_path
 from src.utils.logger import init_logging
 
 with description("main.py") as self:
     with it("runs without parameters"):
-        # main.main()
         expect(True).to(be_true)
+
     with it("executes init_logging"):
         mock_logger = mock(init_logging())
-        # main.main()
-        # expect(mock_logger.called).to(be_false)
