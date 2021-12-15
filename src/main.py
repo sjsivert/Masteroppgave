d56c70475a9f3807053b6d97d07e40c917fba6c4
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-12-15 13:22:48 +0100

    C: Add --save flag to main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+from typing import Tuple
 
 import click
 
@@ -11,9 +12,12 @@ from src.utils.config_parser import config
 
 
 @click.command()
-@click.option("--experiment", "-e", nargs=2, help="Experiment title and description.")
+@click.option("--experiment", "-e", nargs=2, help="Experiment title and description. Title must be unique.")
 @click.option("--is-custom-run", is_flag=True)
-def main(experiment, is_custom_run: bool) -> int:
+@click.option(
+    "--save/--no-save", default=True, help="Boolean flag for saving the results or not. Overrides config.yaml."
+)
+def main(experiment: Tuple[str, str], is_custom_run: bool, save: bool) -> int:
     logger.init_logging()
     logging.info("Started")
 
@@ -26,8 +30,8 @@ def main(experiment, is_custom_run: bool) -> int:
         experiment = Experiment(
             title=experiment[0],
             description=experiment[1],
-            save_sources_to_use=config["experiment"]["save_sources_to_use"].get(),
-            save_source_options=config["experiment"]["save_source"].get(),
+            save_sources_to_use=config["experiment"]["save_sources_to_use"].get() if save else [],
+            save_source_options=config["experiment"]["save_source"].get() if save else {},
         )
 
         experiment.choose_model_structure(config["model"].get())
@@ -36,7 +40,7 @@ def main(experiment, is_custom_run: bool) -> int:
 
         experiment.train_model()
         experiment.test_model()
-        experiment.save_model(options=config.dump())
+        experiment.save_model(options=config.dump()) if save else None
 
     logging.info("Finished")
     return 0
