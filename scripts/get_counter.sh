d921993d875a4e069dcb850104711940da7885d0
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-12-17 14:15:45 +0100

    C: Update get_counter.sh to give whole counter

--- scripts/get_counter.sh
+++ scripts/get_counter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function getApiCount {
-	count=$(curl https://api.countapi.xyz/hit/default/65898bf3-085e-4339-b66f-bef550fc9218 | grep -o -P '\d')
+	count=$(curl https://api.countapi.xyz/get/default/65898bf3-085e-4339-b66f-bef550fc9218 | grep -o -P '\d+')
 }
 getApiCount
 
