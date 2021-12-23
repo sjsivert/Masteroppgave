ca639734789cbc184fed6f74a50e33bc862b5fd7
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-12-23 23:38:31 +0100

    C: Add save_figures to SaveLocalDiskSource. Fixes MAS-13

--- src/save_experiment_source/save_local_disk_source.py
+++ src/save_experiment_source/save_local_disk_source.py
@@ -2,12 +2,16 @@ import logging
 import os
 from typing import List
 
+from matplotlib.figure import Figure
+
 from src.data_types.i_model import IModel
 from src.save_experiment_source.i_save_experiment_source import ISaveExperimentSource
 
 
 class SaveLocalDiskSource(ISaveExperimentSource):
     def __init__(self, model_save_location: str, title) -> None:
+        super().__init__()
+
         self.save_location = model_save_location + title
         try:
             logging.info(f"Saving models to {self.save_location}")
@@ -27,3 +31,17 @@ class SaveLocalDiskSource(ISaveExperimentSource):
     def save_models(self, models: List[IModel]) -> None:
         for idx, model in enumerate(models):
             model.save(self.save_location + f"/model_{idx}.pkl")
+
+    def save_figures(self, figures: List[Figure]) -> None:
+        for idx, figure in enumerate(figures):
+            try:
+                os.mkdir(self.save_location + f"/figures/")
+            except FileExistsError:
+                pass
+            title = _combine_subfigure_titles(figure)
+            figure.savefig(self.save_location + f"/figures/{title}.png")
+
+
+def _combine_subfigure_titles(figure: Figure) -> str:
+    titles = list(map(lambda ax: ax.title.get_text(), figure.axes))
+    return ", ".join(titles)
