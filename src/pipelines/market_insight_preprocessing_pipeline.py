70679f911a5eb5250bb11171a06b078881e491f1
Sindre Sivertsen <sindrejohan1@gmail.com>
2022-01-12 14:16:49 +0100

    Add black formatting ignore pipeline code

--- src/pipelines/market_insight_preprocessing_pipeline.py
+++ src/pipelines/market_insight_preprocessing_pipeline.py
@@ -4,44 +4,21 @@ from src.pipelines import data_loader
 from src.pipelines import market_insight_processing as p
 from src.utils.config_parser import config, get_absolute_path
 
-
+# fmt: off
 def market_insight_pipeline() -> compose.Pipeline:
-    pipeline = compose.Pipeline(
-        steps=[
-            (
-                "load market insight data and categories and merge them",
-                data_loader.load_and_merge_market_insight_and_categories,
-                {
-                    "market_insight_path": get_absolute_path(config["data"]["data_path"].get()),
-                    "categories_path": get_absolute_path(config["data"]["categories_path"].get()),
-                },
-            ),
-            ("convert date columns to date_time format", p.convert_date_to_datetime, {}),
-            ("print data", p.print_df, {}),
-            ("sum up clicks to category level", p.group_by, {"group_by": ["date", "cat_id"]}),
-            (
-                "filter out data from early 2018",
-                p.filter_column,
-                {"column": "date", "value": "2018-12-01"},
-            ),
-            ("print data after 2018 filter", p.print_df, {}),
-            (
-                "drop uninteresting colums",
-                p.drop_columns,
-                {
-                    "columns": [
-                        "internal_doc_id",
-                        "id_x",
-                        "manufacturer_id",
-                        "_version_",
-                        "internal_doc_id",
-                        "id_y",
-                        "adult",
-                    ]
-                },
-            ),
-            ("print data after choosing right columns", p.print_df, {}),
-        ]
-    )
+    pipeline = compose.Pipeline(steps=[
+        ("load market insight data and categories and merge them",
+         data_loader.load_and_merge_market_insight_and_categories,
+         {"market_insight_path": get_absolute_path(config['data']['data_path'].get()),
+          "categories_path": get_absolute_path(config['data']['categories_path'].get())}),
+        ("convert date columns to date_time format", p.convert_date_to_datetime, {}),
+        ("print data", p.print_df, {}),
+        ("sum up clicks to category level", p.group_by, {"group_by": ["date", "cat_id"]}),
+        ("filter out data from early 2018", p.filter_column, {"column": "date", "value": "2018-12-01"}),
+        ("print data after 2018 filter", p.print_df, {}),
+        ("drop uninteresting colums", p.drop_columns,
+         {"columns": ["internal_doc_id", "id_x", "manufacturer_id", "_version_", "internal_doc_id", "id_y", "adult"]}),
+        ("print data after choosing right columns", p.print_df, {}),
+    ])
 
     return pipeline
