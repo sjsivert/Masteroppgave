f929b1db9f1373e9e33a4c885a18b831c481af97
Sindre Sivertsen <sindrejohan1@gmail.com>
2022-01-17 13:57:49 +0100

    ref/fix all old model_type variables to model_structure

--- src/data_types/model_type_enum.py
+++ src/data_types/model_type_enum.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
 
-class ModelTypeEnum(Enum):
+class ModelStructureEnum(Enum):
     validation_model = "validation_model_structure"
     local_univariate_arima = "local_univariate_arima_structure"
