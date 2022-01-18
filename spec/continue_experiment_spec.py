c6480ed081ac9235e45e15ad2c79c003c3c01e4f
Sindre Sivertsen <sindrejohan1@gmail.com>
2022-01-18 11:23:10 +0100

    bug/fix programm crashing if no save_sources given

---
+++ spec/continue_experiment_spec.py
@@ -0,0 +1,7 @@
+from mamba import describe, it
+
+from src.continue_experiment import ContinueExperiment
+
+with describe(ContinueExperiment, "unit") as self:
+    with it("initializes correctly"):
+        pass
