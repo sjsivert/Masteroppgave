8cb852117d696fafe75fa03fcad627a7a3668661
Sindre Sivertsen <sindrejohan1@gmail.com>
2022-01-18 14:27:40 +0100

    feat/Update scripts/execute_exepriment.py to skip commits

--- scripts/execute_experiment.sh
+++ scripts/execute_experiment.sh
@@ -2,6 +2,8 @@
 echo "Experiment title: $1"
 echo "Experiment message: $2"
 
+SKIP_COMMIT=$3
+
 export TERM=xterm
 
 function getApiCount {
@@ -15,6 +17,12 @@ echo $experiment_title
 # Run experiment
 python ./src/main.py --experiment "$experiment_title" "$2"
 
-# Add experiment commit
-git add .
-git commit -m "E: $experiment_title: $2"
+# Skip committing if a third parameter is given to the script
+if [[ -z ${SKIP_COMMIT} ]] ; then
+	# Add experiment commit
+	echo "Committing"
+	git add .
+	git commit -m "E: $experiment_title: $2"
+else
+	echo "Skipping commit"
+fi
