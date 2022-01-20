3f23f01950dd1128ce1628ec6706fcd4abd15867
Sindre Sivertsen <sindrejohan1@gmail.com>
2022-01-20 11:37:47 +0100

    bug/Update add missing IModel functions to SklearnModel

--- src/data_types/sklearn_model.py
+++ src/data_types/sklearn_model.py
@@ -1,4 +1,7 @@
 import pickle
+from typing import Dict
+
+from pandas import DataFrame
 
 from src.data_types.i_model import IModel
 
@@ -7,6 +10,15 @@ class SklearnModel(IModel):
     """
     Wrapper for sklearn models.
     """
+
+    def train(self, data_set: DataFrame, epochs: int = 10) -> Dict:
+        pass
+
+    def test(self, data_set: DataFrame, predictive_period: int = 5) -> Dict:
+        pass
+
+    def visualize(self):
+        pass
 
     def __init__(self, model: object) -> None:
         self.model = model
