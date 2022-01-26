eabc4d475df14a66ff6a54ccdc784673ddbd4c35
Sander Kilen <sander.k.kilen@gmail.com>
2022-01-26 14:26:19 +0100

    test/MAS-50/Validate-neptun-upload-test-through-data-loading

--- spec/save_experiment_source/neptune_save_source_spec.py
+++ spec/save_experiment_source/neptune_save_source_spec.py
@@ -1,14 +1,18 @@
+from pathlib import Path
+
 import expects
-from expects import be, be_false, be_true, expect, match
+from expects import be, be_false, be_true, expect, match, equal
 from genpipes.compose import Pipeline
-from mamba import after, before, description, it
+from mamba import after, before, description, it, _it
 from matplotlib import pyplot as plt
 from mockito import mock
 from sklearn.linear_model import LogisticRegression
 from spec.utils.test_data import test_data
 from src.data_types.sklearn_model import SklearnModel
+from src.data_types.validation_model import ValidationModel
 from src.save_experiment_source.i_log_training_source import ILogTrainingSource
 from src.save_experiment_source.neptune_save_source import NeptuneSaveSource
+from src.utils.file_hasher import generate_file_hash
 from src.utils.temporary_files import temp_files
 
 with description(NeptuneSaveSource, "api") as self:
@@ -21,20 +25,29 @@ with description(NeptuneSaveSource, "api") as self:
             **options,
             title="Test_experiment",
             description="Experiment for automated testing",
+            sync=True,
         )
+
     with after.all:
         self.save_source.close()
 
-    with it("can upload options"):
-        self.save_source._save_options("option1 option2")
+    with it("can upload options, then fetch correct data"):
+        options_data = "option1 option2\noption3 option4"
+        self.save_source._save_options(options_data)
+        fetched_options_data = self.save_source._load_options()
+        expect(fetched_options_data).to(equal(options_data))
 
-    with it("can upload models"):
+    with it("can upload and load models"):
         log_sources = [mock(ILogTrainingSource)]
         models = [
-            SklearnModel(model=LogisticRegression(), log_sources=log_sources),
-            SklearnModel(LogisticRegression(), log_sources),
+            ValidationModel(name="0", log_sources=log_sources),
+            ValidationModel(name="1", log_sources=log_sources),
         ]
         self.save_source._save_models(models)
+        # Load models of ValidationModel should set model variable model_loaded_content to given value
+        self.save_source._load_models(models)
+        for model in models:
+            expect(model.model_loaded_contents).to(equal("Validation model. Mock model saving."))
 
     with it("can track datasets"):
         model_data_save_location = "./models/temp_data"
@@ -47,9 +60,11 @@ with description(NeptuneSaveSource, "api") as self:
                 with open(data_path, "w") as f:
                     f.write(f"Test data containing data from {data_path_name}")
             self.save_source._save_dataset_version(temp_datasets_path)
+            expect(self.save_source._verify_dataset_version(temp_datasets_path)).to(be_true)
 
     with it("can save metrics"):
-        self.save_source._save_metrics({"CPU": {"MAE": 5, "MSE": 6}, "GPU": {"MAE": 6, "MSE": 7}})
+        metrics_data = {"CPU": {"MAE": 5, "MSE": 6}, "GPU": {"MAE": 6, "MSE": 7}}
+        self.save_source._save_metrics(metrics_data)
 
     with it("can can save figures"):
         # Arrange
@@ -70,7 +85,7 @@ with description(NeptuneSaveSource, "api") as self:
             experiment_tags=["test"],
         )
 
-    with it("can continue previous experiment"):
+    with _it("can continue previous experiment"):
         first_run = NeptuneSaveSource(
             project_id=self.project_id,
             title="Test continue from last run",
