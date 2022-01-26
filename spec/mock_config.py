18bdf037001ff6649f8a623e5c4a7dcdbc3342ef
Sindre Sivertsen <sindrejohan1@gmail.com>
2022-01-26 15:00:08 +0100

    feat/clean up mock_config.py

--- spec/mock_config.py
+++ spec/mock_config.py
@@ -6,29 +6,30 @@ def init_mock_config(
 ):
     config.clear()
     config.read(user=False)
-    config["experiment"] = {
-        "tags": ["tag3", "tag4"],
-        "save_sources_to_use": ["disk"],
-        "checkpoint_save_location": "./models/temp-checkpoints",
-        "log_model_every_n_epoch": 1,
-        "save_source": {
-            "disk": {
-                "model_save_location": model_save_location,
+    config.set(
+        {
+            "experiment": {
+                "tags": ["tag3", "tag4"],
+                "save_sources_to_use": ["disk"],
+                "checkpoint_save_location": "./models/temp-checkpoints",
+                "log_model_every_n_epoch": 1,
+                "save_source": {
+                    "disk": {
+                        "model_save_location": model_save_location,
+                    },
+                    "neptune": {"project_id": "sjsivertandsanderkk/Masteroppgave"},
+                },
             },
-            "neptune": {"project_id": "sjsivertandsanderkk/Masteroppgave"},
-        },
-    }
-    config["experiment"]["save_sources_to_use"] = ["disk"]
-    config["experiment"]["save_source"]["disk"]["model_save_location"] = model_save_location
-
-    config["model"] = {
-        "model_type": model_struct_type,
-        "rng_seed": 42,
-        "local_univariate_arima": {
-            "order": (1, 1, 1),
-        },
-    }
-    config["data"] = {
-        "data_path": "./README.md",
-        "categories_path": "./README.md",
-    }
+            "model": {
+                "model_type": model_struct_type,
+                "rng_seed": 42,
+                "local_univariate_arima": {
+                    "order": (1, 1, 1),
+                },
+            },
+            "data": {
+                "data_path": "./README.md",
+                "categories_path": "./README.md",
+            },
+        }
+    )
