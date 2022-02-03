6bf021cd42f4f16faa6c16e86dd7e5b0b77a2a54
Sindre Sivertsen <sindrejohan1@gmail.com>
2022-02-03 12:09:29 +0100

    bug/Fix configurable training_size for local_univariate_arima

--- src/pipelines/local_univariate_arima_pipeline.py
+++ src/pipelines/local_univariate_arima_pipeline.py
@@ -3,20 +3,24 @@ from genpipes.compose import Pipeline
 from src.pipelines import market_insight_processing as market_processing
 
 
-def local_univariate_arima_pipeline(pipeline: Pipeline) -> Pipeline:
+def local_univariate_arima_pipeline(pipeline: Pipeline, training_size: float) -> Pipeline:
     """
     Datapipeline which processes the data to be on the correct format
     before the local_univariate_arima model can be applied.
 
     Args:
-        pipeline (Pipeline): The pipeline to which the steps will be added.
+        :param pipeline:
+        :param training_size:
     Returns:
         Pipeline: The pipeline with the steps added.
     """
     return Pipeline(steps=
                     pipeline.steps + [
-                        ("filter out category 'Nettverkskabler' (11573)", market_processing.filter_by_cat_id, {"cat_id": 11573}),
-                        ("choose columns 'hits' and 'date'", market_processing.choose_columns, {"columns": ["date", "hits"]}),
+                        ("filter out category 'Nettverkskabler' (11573)", market_processing.filter_by_cat_id,
+                         {"cat_id": 11573}),
+                        ("choose columns 'hits' and 'date'", market_processing.choose_columns,
+                         {"columns": ["date", "hits"]}),
                         ("fill in dates with zero values", market_processing.fill_in_dates, {}),
-                        ("split up into training set and test set", market_processing.split_into_training_and_test_set, {"training_size": 0.8}),
+                        (f"split up into training set ({training_size}) and test set ({1 - training_size})",
+                         market_processing.split_into_training_and_test_set, {"training_size": training_size}),
                     ])
