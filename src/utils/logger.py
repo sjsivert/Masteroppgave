ccbb2b25a1a75270b77882b2e7ed2b91a4caf43e
Sindre Sivertsen <sindrejohan1@gmail.com>
2022-02-03 15:59:58 +0100

    feat/MAS-71/Add feature to save logging output

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,9 +1,15 @@
 import logging
 
 
-def init_logging():
+def init_logging(log_level: str = "INFO", log_file: str = "") -> None:
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.getLevelName(log_level),
         format="[%(asctime)s] %(levelname)s {%(module)s} [%(funcName)s] %(message)s",
         datefmt="%Y-%m-%d,%H:%M:%S:%f",
     )
+    if log_file != None and log_file != "":
+        logger = logging.getLogger()
+        # TODO: Make log file configurable
+        handler = logging.FileHandler("./log_file.log")
+        logger.addHandler(handler)
+    logging.info(f"Logger initialized. Log level: {log_level}, log file: {log_file}")
