559be0479894cf963d44d226ff1b0b0f50d8fe90
Sindre Sivertsen <sindrejohan1@gmail.com>
2022-02-04 11:44:11 +0100

    ref/Refactor interfaces to be abstract

--- src/data_types/i_model.py
+++ src/data_types/i_model.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+
+from abc import ABCMeta, abstractmethod
 from typing import List, Dict
 from pandas.core.series import Series
 
@@ -8,61 +10,56 @@ from pandas.core.frame import DataFrame
 from src.save_experiment_source.i_log_training_source import ILogTrainingSource
 
 
-class IModel:
+class IModel(metaclass=ABCMeta):
     """
     Interface for all models to implement in order to save and load
     """
 
-    def __init__(self, log_sources: List[ILogTrainingSource], name: str = "placeholder"):
-        # Temporary logging
-        self.log_sources: List[ILogTrainingSource] = log_sources
-        # Model name
-        self.name: str = name
-        # Predictions
-        self.training_value_aproximation: Series = Series()
-        self.predictions: Series = Series()
-        # Visualization
-        self.figures: List[Figure] = []
-        self.metrics: Dict = {}
+    @abstractmethod
+    def get_name(self) -> str:
+        pass
 
-    def get_name(self):
-        return self.name
-
+    @abstractmethod
     def train(self, data_set: DataFrame, epochs: int = 10) -> Dict:
         """
         Train the model.
         Return dict with training accuracy and error metric
         """
-        raise NotImplementedError()
+        pass
 
+    @abstractmethod
     def test(self, data_set: DataFrame, predictive_period: int = 5) -> Dict:
         """
         Test the trained model with test set
         Validate through prediction
         """
-        raise NotImplementedError()
+        pass
 
+    @abstractmethod
     def get_figures(self) -> List[Figure]:
         """
         Return a list of figures created by the model for visualization
         """
         pass
 
+    @abstractmethod
     def get_metrics(self) -> Dict:
         """
         Fetch metrics from model training or testing
         """
-        raise NotImplementedError()
+        pass
 
+    @abstractmethod
     def save(self, path: str) -> str:
         """
         Save the model to the specified path.
         :returns: Path to saved model file
         """
-        raise NotImplementedError()
+        pass
 
+    @abstractmethod
     def load(self, path: str) -> IModel:
         """
         Load the model from the specified path.
         """
-        raise NotImplementedError()
+        pass
--- src/model_strutures/i_model_structure.py
+++ src/model_strutures/i_model_structure.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from abc import ABCMeta, abstractmethod
 from typing import Dict, List, Optional
 
 from genpipes.compose import Pipeline
@@ -9,22 +10,20 @@ from src.data_types.i_model import IModel
 from src.save_experiment_source.i_log_training_source import ILogTrainingSource
 
 
-class IModelStructure:
+class IModelStructure(metaclass=ABCMeta):
     """
     Interface for all model types.
     Contains methods shared by all.
     """
 
-    def __init__(self, log_sources: List[ILogTrainingSource]):
-        # This is an interface.
-        pass
-
+    @abstractmethod
     def init_models(self, load: bool = False):
         """
         Initialize models in the structure
         """
         pass
 
+    @abstractmethod
     def process_data(self, data_pipeline: Pipeline) -> Optional[DataFrame]:
         """
         Processes data to get it on the correct format for the relevant model.
@@ -33,48 +32,56 @@ class IModelStructure:
         """
         pass
 
+    @abstractmethod
     def get_data_pipeline(self) -> Pipeline:
         """
         Returns the data pipeline used to process the data.
         """
         pass
 
+    @abstractmethod
     def train(self) -> IModelStructure:
         """
         Trains the model.
         """
         pass
 
+    @abstractmethod
     def test(self) -> Dict:
         """
         Tests the model.
         """
         pass
 
+    @abstractmethod
     def auto_tuning(self, Dict) -> None:
         """
         Automatic tuning of the model
         """
         pass
 
+    @abstractmethod
     def get_models(self) -> List[IModel]:
         """
         Return the modes contained in the structure
         """
         pass
 
+    @abstractmethod
     def get_metrics(self) -> Dict:
         """
         Returns dict of metrics
         """
         pass
 
+    @abstractmethod
     def get_figures(self) -> List[Figure]:
         """
         Returns list of figures
         """
         pass
 
+    @abstractmethod
     def get_tuning_parameters(self) -> Dict:
         """
         Returns a dict with info regarding the automatic tuning of the models
