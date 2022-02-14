ee6791f4e8568fedafd6728c2bcb1bb2331aabc9
Sindre Sivertsen <sindrejohan1@gmail.com>
2022-02-14 18:04:25 +0100

    feat/Add profiler for analyzing code execution

---
+++ src/utils/profiler.py
@@ -0,0 +1,36 @@
+import cProfile
+import io
+import logging
+import pstats
+from cgitb import enable
+from typing import Callable, TypeVar
+
+T = TypeVar("T")
+
+
+def profiler(fnc: Callable[..., T], enable: bool = True) -> T:
+    """
+    A decorator that uses cProfile to profile an execution of a function
+    """
+
+    def inner(*args, **kwargs):
+        if enable:
+            profiler = cProfile.Profile()
+            profiler.enable()
+            # Call the function
+            return_value = fnc(*args, **kwargs)
+            profiler.disable()
+
+            stream = io.StringIO()
+            sortby = "cumulative"
+            ps = pstats.Stats(profiler, stream=stream).sort_stats(sortby)
+            # logging.INFO(ps.print_stats())
+            ps.print_stats()
+            print(stream.getvalue())
+            return return_value
+        else:
+            # Call the function
+            return_value = fnc(*args, **kwargs)
+            return return_value
+
+    return inner
