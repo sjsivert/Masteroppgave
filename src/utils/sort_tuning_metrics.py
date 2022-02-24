b667b0618d04f5e9a9c8bc19e8f745b87e5f7c4b
Sander Kilen <sander.k.kilen@gmail.com>
2022-02-24 17:56:50 +0100

    exp/ARIMA-dataset-2-SMAPE

---
+++ src/utils/sort_tuning_metrics.py
@@ -0,0 +1,50 @@
+from typing import Dict
+import os
+import csv
+
+
+def load_dict(path: str) -> Dict[str, Dict[str, Dict[str, float]]]:
+    if not os.path.isfile(path):
+        raise FileNotFoundError(f"File not found at path: {path}")
+    with open(path, "r") as f:
+        tuning_metrics = {}
+        reader = csv.reader(f, delimiter=";")
+        next(reader)  # Skip row with column names
+        for row in reader:
+            if row[0] not in tuning_metrics:
+                tuning_metrics[row[0]] = {}
+            # Convert from string to dict of string float set values
+            error_metrics_set_list = dict(
+                (i, float(j)) for i, j in [x.split(":") for x in row[2].split(",")]
+            )
+            tuning_metrics[row[0]][row[1]] = error_metrics_set_list
+        return tuning_metrics
+
+
+def sort_dict(metrics: Dict, metric_name: str) -> Dict:
+    sorted_metrics = {}
+    for dataset_name, param_metrics in metrics.items():
+        sorted_param_dict = dict(sorted(param_metrics.items(), key=lambda x: x[1][metric_name], reverse=False))
+        sorted_metrics[dataset_name] = sorted_param_dict
+    return sorted_metrics
+
+
+def store_sorted_metrics(metrics: Dict, path):
+    with open(path, "w") as f:
+        for dataset_name, param_metrics in metrics.items():
+            f.write(f"Model: {dataset_name}\n")
+            for param, metric_set in param_metrics.items():
+                metric_str = ", ".join([f"{x}:{str(y)}" for x,y in metric_set.items()])
+                f.write(f"{param} -> {metric_str} \n")
+            f.write("\n")
+
+
+if __name__ == "__main__":
+    metric_name = "MASE"
+    experiment_name = "arima_tuning_dataset_2"
+    metrics_file_path = f"./models/{experiment_name}/logging/tuning_metrics.csv"
+    sorted_save_path = f"./models/{experiment_name}/tuning_{metric_name}.txt"
+
+    metrics_dict = load_dict(metrics_file_path)
+    sorted_metrics_dict = sort_dict(metrics_dict, metric_name)
+    store_sorted_metrics(sorted_metrics_dict, sorted_save_path)
