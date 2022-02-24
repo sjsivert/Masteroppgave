4637753ab564af22eddd4b166e9b87b6c124aa37
Sindre Sivertsen <sindrejohan1@gmail.com>
2022-02-24 11:30:42 +0100

    exp/config-before-experient

--- scripts/execute_experiment.sh
+++ scripts/execute_experiment.sh
@@ -10,7 +10,7 @@ function getApiCount {
      count=$(curl https://api.countapi.xyz/hit/default/65898bf3-085e-4339-b66f-bef550fc9218 | grep -o -P '\d+')
 }
 getApiCount
-experiment_title="$1$count"
+experiment_title="$1-[$count]"
 
 echo $experiment_title
 
