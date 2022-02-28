a4719f2581da79c47bc6523f13b6c46216945ee7
Sander Kilen <sander.k.kilen@gmail.com>
2022-02-28 15:21:51 +0100

    feat/Utils-methods-for-creating-results-tables

---
+++ utils/experiment_results.py
@@ -0,0 +1,68 @@
+"""
+Extract experiment results from model.
+Create table with data from the ran models with parameters
+1. One method for ARIMA
+"""
+from typing import Dict
+
+import yaml
+from pandas import DataFrame
+import utils
+
+
+experiment_path = "./models/"
+experiment_name = "arima_experiment_dataset_2_MASE"
+table_save_location = "./MastersThesis/tables/results"
+table_name = "ARIMA dataset 2"
+table_description = "experiment_arima_dataset2"
+
+
+def config_extraction_arima():
+    with open(f"{experiment_path}{experiment_name}/options.yaml") as f:
+        config = yaml.full_load(f)
+        params = config["model"]["local_univariate_arima"]["model_structure"]
+    return params
+
+
+# Extract ARIMA data
+def arima_exp_results_table() -> Dict[str, Dict[str, float]]:
+    # Get config from experiment
+    metrics = {}
+    with open(f"{experiment_path}{experiment_name}/metrics.txt") as f:
+        content = f.readlines()
+        current_model = None
+        for line in content:
+            line = line.strip("\n")
+            if "Average" in line:  # No more data, average always last
+                break
+            elif "___" in line:  # A new model is used
+                current_model = line.strip("_").split("-")[1]
+                metrics[current_model] = {}
+                continue
+            elif current_model and line:
+                metric_line = line.split(":")
+                metric_value = float(metric_line[1].strip(" "))
+                metric_name = metric_line[0].split("_")[1]
+                metrics[current_model][metric_name] = metric_value
+    return metrics
+
+
+def arima_merge_metrics_and_params():
+    config = config_extraction_arima()
+    metrics = arima_exp_results_table()
+    for model in config:
+        dataset_id = model["time_series_id"]
+        for param, val in model["hyperparameters"].items():
+            metrics[str(dataset_id)][str(param)] = int(val)
+    return metrics
+
+
+def arima_dataframe() -> DataFrame:
+    arima_metrics = arima_merge_metrics_and_params()
+    metrics_data_frame = DataFrame(arima_metrics).transpose()
+    return metrics_data_frame
+
+
+if __name__ == "__main__":
+    metrics = arima_dataframe()
+    utils.dataframe_to_latex_tabular(metrics, table_name, table_description, add_index=True, save_local=table_save_location)
---
+++ utils/utils.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import shutil
+from typing import List
+
+# TODO: Replace with config value
+figure_save_location = "../MScTemplate/figs/code_generated/data_exploration/"
+def save_fig_for_raport(fig: plt.Axes, name: str) -> plt.Axes:
+  """
+  Save figure for raport with given name
+  
+  Parameters:
+    fig (MplAxes): Figure to save
+    name (str): Name of the figure
+    
+  Returns:
+    MplAxes: Original figure
+  """
+  plt.figure()
+  ax = fig
+  plt.rcParams['font.size'] = '16'
+  plt.tick_params(labelsize=20)
+  ax.set_title(name, fontsize=20)
+  return ax.get_figure().savefig(f"{figure_save_location}/{name}.png", bbox_inches='tight', facecolor='w', edgecolor='w')
+
+
+# TODO: Replace with config value
+table_save_location = "../MScTemplate/tables/code_generated/data_exploration/"
+def dataframe_to_latex_tabular(df: pd.DataFrame, caption: str, label: bool, add_index=False, save_local=table_save_location) -> pd.DataFrame:
+    """
+    Save dataframe to latex tabular tex file for raport
+    
+    Parameters:
+      df (pd.Dataframe): Dataframe to save
+      caption (str): Caption of the table
+      label (bool): If true, add label to table
+      add_index (bool): If true, add index to table
+
+    Returns:
+      pd.DataFrame: Original dataframe
+    """
+    table_string = df.to_latex(
+        index=add_index,
+        bold_rows=True,
+        caption=caption,
+        label=f"table:{label}",
+        header=True,
+        multicolumn=True,
+        multirow=True,
+        multicolumn_format="c",
+        # Dont know if this works yet
+        position="h",
+        )
+    table_split = table_string.split("\n")
+    table_join = "\n".join(table_split)
+    with open(f"{save_local}/{label}.tex", "w") as f:
+          f.write(table_join)
+
+
+# Copy experiment figures from experiments
+thesis_figure_location = "./MastersThesis/figs/results"
+figure_name = ["Predictions", "Data Prediction"]
+def transfer_experiment_figures(experiment: str, figure_types: List[str]):
+    # Check if experiment exists and experiment has figures folder
+    if not os.path.isdir(f"./models/{experiment}") or not os.path.isdir(f"./models/{experiment}/figures"):
+        raise NotADirectoryError(f"The experiment {experiment} does not exist or have a figure directory.")
+    # Check that the folder to store files exists
+    if not os.path.isdir(thesis_figure_location):
+        raise NotADirectoryError(f"The directory for storing result images does not exist. Create a driectory at: {thesis_figure_location}")
+    if not os.path.isfile(f"{thesis_figure_location}/{experiment}"):
+        os.mkdir(f"{thesis_figure_location}/{experiment}")
+
+    # Figure names are [Predictions, Training data, Training data approximation, Data Prediction
+    experiment_figures = os.listdir(f"./models/{experiment}/figures")
+    for image_name in experiment_figures:
+        if any([x in image_name for x in figure_types]):
+            # Copy file from src to destination
+            shutil.copyfile(
+                f"./models/{experiment}/figures/{image_name}",
+                f"{thesis_figure_location}/{experiment}/{image_name}"
+            )
