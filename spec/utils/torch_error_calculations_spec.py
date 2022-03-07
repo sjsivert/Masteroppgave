be53b104025d9a04e7e8605944832a7afe642ab8
Sander Kilen <sander.k.kilen@gmail.com>
2022-03-07 11:09:38 +0100

    feat/MASE-error-metrics

--- spec/utils/torch_error_calculations_spec.py
+++ spec/utils/torch_error_calculations_spec.py
@@ -12,71 +12,47 @@ from spec.mock_config import init_mock_config
 from src.utils import config_parser
 from src.utils.pytorch_error_calculations import *
 
-with description("error_calculations", "unit") as self:
-    """
+with description("error_calculations", "this") as self:
     with before.each:
         init_mock_config()
         config["experiment"].set({"error_metrics": ["MSE", "MAE"]})
-    """
-
-    with shared_context("mock_dataset_basic"):
-        data_set = torch.tensor([[[1], [2], [3], [4]]], dtype=torch.float64)
-        proposed_data = torch.tensor([[[1], [2], [4], [4]]], dtype=torch.float64)
 
     with shared_context("mock_dataset"):
-        data_set = torch.tensor([[[2], [4], [1]]], dtype=torch.float64)
-        proposed_data = torch.tensor([[[3], [2], [1]]], dtype=torch.float64)
+        targets = torch.tensor([[[2], [4], [1]]], dtype=torch.float64)
+        predictions = torch.tensor([[[3], [2], [1]]], dtype=torch.float64)
 
     with it("can calculate MAE"):
-        with included_context("mock_dataset_basic"):
-            expected_mae = 0.25
-            mae = calculate_mae(data_set, proposed_data)
-            expect(mae.item()).to(equal(expected_mae))
+        with included_context("mock_dataset"):
+            expected_mae = 1.0
+            mae = calculate_mae(targets, predictions)
+            expect(round(mae.item(), 4)).to(equal(expected_mae))
 
     with it("can calculate MSE"):
-        with included_context("mock_dataset_basic"):
-            expected_mse = 0.25
-            mse = calculate_mse(data_set, proposed_data)
-            expect(mse.item()).to(equal(expected_mse))
+        with included_context("mock_dataset"):
+            expected_mse = round(5 / 3, 4)
+            mse = calculate_mse(targets, predictions)
+            expect(round(mse.item(), 4)).to(equal(expected_mse))
 
-    """
     with it("can caluclate MASE"):
         with included_context("mock_dataset"):
-            expected_mase = 0.4
-            mase = calculate_mase(data_set, proposed_data)
+            expected_mase = 0.6
+            mase = calculate_mase(targets, predictions)
             expect(round(mase.item(), 4)).to(equal(expected_mase))
-    """
 
     with it("can caluclate SMAPE"):
         with included_context("mock_dataset"):
             expected_smape = 0.3493
-            smape = calculate_smape(data_set, proposed_data)
+            smape = calculate_smape(targets, predictions)
             expect(round(smape.item(), 4)).to(equal(expected_smape))
-
-    """
-    with it("can caluclate OWA"):
-        with included_context("mock_dataset"):
-            data_set = DataFrame(
-                [
-                    2,
-                    4,
-                    1,
-                ]
-            )
-            proposed_data = DataFrame([3, 2, 1])
-            expected_smape = 0.585
-            smape = calculate_owa(data_set, proposed_data)
-            expect(round(smape, 4)).to(equal(expected_smape))
 
     with it("can calculate errors"):
         with included_context("mock_dataset"):
-            errors = calculate_error(data_set, proposed_data)
+            errors = calculate_errors(targets, predictions)
             expect(("MAE" and "MSE") in errors).to(be_true)
 
     with it("can ignore wrong configuration"):
         with included_context("mock_dataset"):
             config["experiment"].set({"error_metrics": ["not a valid metric", "MAE"]})
-            errors = calculate_error(data_set, proposed_data)
+            errors = calculate_errors(targets, predictions)
             expect("MAE" in errors).to(be_true)
             expect(len(errors.keys())).to(equal(1))
-    """
