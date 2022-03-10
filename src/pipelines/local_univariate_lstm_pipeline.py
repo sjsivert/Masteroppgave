58ecc450a07926e689397db92610dbb895c76291
Sindre Sivertsen <sindrejohan1@gmail.com>
2022-03-10 16:48:58 +0100

    exp/Test tuning towards training error

--- src/pipelines/local_univariate_lstm_pipeline.py
+++ src/pipelines/local_univariate_lstm_pipeline.py
@@ -38,7 +38,7 @@ def local_univariate_lstm_pipeline(
             ("choose columns 'interest' and 'date'", market_processing.choose_columns, {"columns": ["date", "interest"]}),
             ("fill in dates with zero values", market_processing.fill_in_dates, {}),
             ("convert to np.array", market_processing.convert_to_np_array, {}),
-            (f"scale data between -1 and 1", market_processing.scale_data, {"should_scale": True}),
+            (f"scale data between -1 and 1", market_processing.scale_data, {"should_scale": False}),
             (f"split up into training set ({training_size}) and test set ({1 - training_size})",
              market_processing.split_into_training_and_test_set,
                 {"training_size": training_size}),
