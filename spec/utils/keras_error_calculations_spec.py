066d241e2efcc56b00c3ed0d65f187835750c75f
Sindre Sivertsen <sindrejohan1@gmail.com>
2022-03-30 14:05:05 +0200

    feat/Keras implement smape and mase errors

--- spec/utils/keras_error_calculations_spec.py
+++ spec/utils/keras_error_calculations_spec.py
@@ -1,8 +1,9 @@
 import confuse
+import tensorflow as tf
 from confuse import Configuration
 from confuse.exceptions import NotFoundError
 from expects import be_true, equal, expect
-from mamba import after, before, description, included_context, it, shared_context
+from mamba import _it, after, before, description, included_context, it, shared_context
 from mockito import unstub, verify, when
 from pandas import DataFrame
 from spec.mock_config import init_mock_config
@@ -12,9 +13,11 @@ from src.utils.error_calculations import *
 from src.utils.keras_error_calculations import (
     config_metrics_to_keras_metrics,
     generate_error_metrics_dict,
+    keras_mase,
+    keras_smape,
 )
 
-with description("Keras_error_calculations.py", "this") as self:
+with description("Keras_error_calculations.py", "unit") as self:
 
     with it("generate_error_metrics_dict works as expected"):
         init_mock_config()
@@ -40,11 +43,25 @@ with description("Keras_error_calculations.py", "this") as self:
         expected_result = [
             "mean_squared_error",
             "mean_absolute_error",
-            "mean_absolute_scaled_error",
-            "symetric_mean_absolute_percentage_error",
+            keras_mase,
+            keras_smape,
         ]
 
         # Act
         result = config_metrics_to_keras_metrics()
 
         expect(result).to(equal(expected_result))
+
+    with _it("can caluclate MASE"):
+        # TODO: Test if it calculates MASE correctly!
+        data_set = np.array(
+            [
+                2,
+                4,
+                1,
+            ]
+        )
+        proposed_data = np.array([3, 2, 1])
+        expected_mase = 0.4
+        mase = keras_mase(data_set, proposed_data)
+        expect(round(mase, 4)).to(equal(expected_mase))
