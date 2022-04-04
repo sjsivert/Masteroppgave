38c3e0f1e2adfb072ba12aecf01e67ed4c15c90a
Sindre Sivertsen <sindrejohan1@gmail.com>
2022-04-04 12:38:42 +0200

    feat/Add calcultate season feature generator

---
+++ spec/pipelines/date_feature_generator_spec.py
@@ -0,0 +1,20 @@
+import datetime
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+from expects import be_true, equal, expect
+from genpipes import compose
+from genpipes.compose import Pipeline
+from mamba import _it, before, description, included_context, it, shared_context
+from pandas import DataFrame, Timestamp
+from src.pipelines.date_feature_generator import calculate_season
+
+with description("date_feature_generator", "this") as self:
+    with it("can calculate the season"):
+        months = [(datetime(2000, x % 12 + 1, 1)) for x in range(0, 24)]
+        result = list(map(lambda month: calculate_season(month), months))
+        expect(max(result)).to(equal(1))
+        expect(min(result)).to(equal(-1))
+        expect(calculate_season(datetime(2000, 12, 1))).to(equal(1))
+        expect(calculate_season(datetime(2000, 6, 1))).to(equal(-1))
